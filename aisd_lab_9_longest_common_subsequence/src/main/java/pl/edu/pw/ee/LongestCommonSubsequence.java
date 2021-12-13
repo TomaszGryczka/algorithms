d29251397c73315c7840807c2aed7f3b435046b6
TomaszGryczka <tumek001@gmail.com>
2021-12-13 09:46:09 +0100

    first version of lcs algorithm

---
+++ aisd_lab_9_longest_common_subsequence/src/main/java/pl/edu/pw/ee/LongestCommonSubsequence.java
@@ -0,0 +1,18 @@
+package pl.edu.pw.ee;
+
+public class LongestCommonSubsequence {
+
+    public LongestCommonSubsequence(String firstStr, String secondStr) {
+        
+    }
+
+    public String findLCS() {
+
+        return null;
+    }
+
+    public void display() {
+
+    }
+
+}
