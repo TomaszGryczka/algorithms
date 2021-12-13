1a00b15f6a350c21c4422dacce4723b2f58489c7
TomaszGryczka <tumek001@gmail.com>
2021-12-13 23:53:34 +0100

    added field and arrow classes

---
+++ aisd_lab_9_longest_common_subsequence/src/main/java/pl/edu/pw/ee/Field.java
@@ -0,0 +1,62 @@
+package pl.edu.pw.ee;
+
+public class Field implements Comparable<Field> {
+    private int fieldValue;
+
+    private Arrow arrow;
+
+    public Field(int fieldValue, Arrow arrow) {
+        this.fieldValue = fieldValue;
+        this.arrow = arrow;
+    }
+
+    @Override
+    public int compareTo(Field otherField) {
+        if (fieldValue < otherField.fieldValue) {
+            return -1;
+        } else if (fieldValue > otherField.fieldValue) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+
+        if (this == obj) {
+            return true;
+        }
+
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+
+        if (this.fieldValue != ((Field) obj).fieldValue) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int primeNum = 31;
+        int result = 1;
+
+        result = primeNum * result + fieldValue;
+
+        return result;
+    }
+
+    public int getFieldValue() {
+        return fieldValue;
+    }
+
+    public Arrow getArrow() {
+        return arrow;
+    }
+}
